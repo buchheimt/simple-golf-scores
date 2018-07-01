e6bb47d9436d44f60be8d7606111e8d42af5b69c
Tyler <tbuchhei@alumni.nd.edu>
2018-06-30 21:20:40 -0500

    Layout basic scorecard structure

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 
-import { scorecard, scorecardHeader, courseName } from './styles';
+import { scorecard, scorecardHeader, courseName, scorecardRow, holesContainer, holeColumn, holeRow, parRow } from './styles';
+
+const rowConfig = [
+  {value: 1, label: 'Blue'},
+  {value: 2, label: 'White'},
+  {value: 3, label: 'Red'},
+  {value: 4, label: 'Par'},
+  {value: 5, label: 'Rank'}
+]
+
+const holes = [
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1},
+  {yardage: 444, par: 3, rank: 18, num: 1}
+]
 
 class Scorecard extends Component {
 
@@ -11,6 +40,46 @@ class Scorecard extends Component {
         <div className={scorecardHeader} >
           <h1 className={courseName}>Green Crest GC</h1>
         </div>
+        <div className={holesContainer} >
+          <div className={holeColumn}>
+            <div className={holeRow}>Hole</div>
+            <p>Yardage</p>
+            <p className={parRow}>Par</p>
+            <p>Rank</p>
+            <p>Player 1</p>
+            <p>Player 2</p>
+            <p>Player 3</p>
+            <p>Player 4</p>
+          </div>
+          {holes.slice(0, 9).map(hole => (
+            <div className={holeColumn}>
+              <div className={holeRow}>{hole.num}</div>
+              <p>{hole.yardage}</p>
+              <p className={parRow}>{hole.par}</p>
+              <p>{hole.rank}</p>
+              <p> </p>
+            </div>
+          ))}
+          <div className={holeColumn}>
+            <p className={holeRow}>Totals</p>
+          </div>
+          {holes.slice(9).map(hole => (
+            <div className={holeColumn}>
+              <div className={holeRow}>{hole.num}</div>
+              <p>{hole.yardage}</p>
+              <p className={parRow}>{hole.par}</p>
+              <p>{hole.rank}</p>
+              <p> </p>
+            </div>
+          ))}
+          <div className={holeColumn}>
+            <div className={holeRow}>Totals</div>
+          </div>
+          <div className={holeColumn}>
+            <div className={holeRow}>Round</div>
+          </div>
+        </div>
+       
       </div>
     )
   }
--- src/styles.js
+++ src/styles.js
@@ -9,11 +9,11 @@ const COLORS = {
 }
 
 export const scorecard = css`
-  width: 75%;
+  display: inline-block;
   margin: 50px auto 0;
   height: 400px;
-  box-shadow: 1px 3px 3px 3px darkgray;
-  background-color: ${COLORS.medium};
+  box-shadow: 1px 3px 3px 3px #444;
+  background-color: ${COLORS.light};
   border-radius: 5px;
 `;
   
@@ -23,9 +23,42 @@ export const scorecardHeader = css`
   align-items: center;
   background-color: ${COLORS.light_medium};
   padding: 15px 0;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
 `;
+
+export const holeRow = css`
+  background-color: ${COLORS.dark_medium};
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 4px;
+  width: 100%;
+`;
+
+export const parRow = css`
+  background-color: ${COLORS.medium};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 4px;
+  width: 100%;
+`
 
 export const courseName = css`
   font-size: 16px;
   color: white;
 `;
+
+export const holesContainer = css`
+  display: flex;
+  border: 1px solid orange;
+`
+
+export const holeColumn = css`
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
